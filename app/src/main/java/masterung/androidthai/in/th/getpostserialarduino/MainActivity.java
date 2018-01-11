5355839a27524bee336b36ac79887620504983b9
MasterUNG <phrombutr@gmail.com>
2018-01-11 16:03:52 +0700

    finish 11JAn

--- app/src/main/java/masterung/androidthai/in/th/getpostserialarduino/MainActivity.java
+++ app/src/main/java/masterung/androidthai/in/th/getpostserialarduino/MainActivity.java
@@ -1,31 +1,108 @@
 package masterung.androidthai.in.th.getpostserialarduino;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.zxing.Result;
+import com.physicaloid.lib.Physicaloid;
+import com.physicaloid.lib.usb.driver.uart.ReadLisener;
 
 import masterung.androidthai.in.th.getpostserialarduino.fragment.MainFragment;
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 public class MainActivity extends AppCompatActivity{
 
+    private Physicaloid physicaloid;
+    private String resultString;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
+//        Add Fragment
+//        addFragment(savedInstanceState);
 
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.contentMainFragment, new MainFragment())
-                    .commit();
-
-        }
+//        Receive Arduino
+        receiveArduino();
 
 
     }   // Main Method
+
+
+
+    private void receiveArduino() {
+
+        try {
+
+            physicaloid = new Physicaloid(MainActivity.this);
+
+            if (physicaloid.open()) {
+
+                physicaloid.addReadListener(new ReadLisener() {
+                    @Override
+                    public void onRead(int i) {
+
+                        byte[] bytes = new byte[i];
+                        physicaloid.read(bytes, i);
+                        resultString = new String(bytes);
+                        showMessage("Result ==> " + resultString);
+
+                        if (resultString.equals("9")) {
+                            openScanQRcode();
+                        }
+
+                    }
+                });
+
+            } else {
+                showMessage("physicoloid Close");
+            }
+
+        } catch (Exception e) {
+            showMessage("Error ==> " + e.toString());
+        }
+
+
+    }
+
+    private void showMessage(String messageSting) {
+
+        Toast.makeText(MainActivity.this, messageSting,
+                Toast.LENGTH_LONG).show();
+
+    }
+
+    public void clickTestReceiveByArduion(View view) {
+        openScanQRcode();
+    }
+
+    private void openScanQRcode() {
+
+        Toast.makeText(MainActivity.this, getString(R.string.receive_ok),
+                Toast.LENGTH_SHORT).show();
+
+
+        Intent intent = new Intent(MainActivity.this, ScanCodeActivity.class);
+        startActivity(intent);
+
+
+    }   // openScanQRcode
+
+
+
+//    private void addFragment(Bundle savedInstanceState) {
+//        if (savedInstanceState == null) {
+//            getSupportFragmentManager().beginTransaction()
+//                    .add(R.id.contentMainFragment, new MainFragment())
+//                    .commit();
+//        }
+//    }
 
 
 }   // Main Class
--- app/src/main/java/masterung/androidthai/in/th/getpostserialarduino/ScanCodeActivity.java
+++ app/src/main/java/masterung/androidthai/in/th/getpostserialarduino/ScanCodeActivity.java
@@ -3,8 +3,11 @@ package masterung.androidthai.in.th.getpostserialarduino;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.zxing.Result;
+import com.physicaloid.lib.Physicaloid;
+import com.physicaloid.lib.usb.driver.uart.ReadLisener;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
@@ -13,6 +16,8 @@ public class ScanCodeActivity extends AppCompatActivity implements ZXingScannerV
     private ZXingScannerView zXingScannerView;
     private String resultString;
     private String tag = "11JanV1";
+    private Physicaloid physicaloid;
+    private int bandAnInt = 9600;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,9 @@ public class ScanCodeActivity extends AppCompatActivity implements ZXingScannerV
 
         zXingScannerView = new ZXingScannerView(ScanCodeActivity.this);
         setContentView(zXingScannerView);
+
+        physicaloid = new Physicaloid(ScanCodeActivity.this);
+        physicaloid.setBaudrate(bandAnInt);
 
 
     }   // Main Method
@@ -47,11 +55,64 @@ public class ScanCodeActivity extends AppCompatActivity implements ZXingScannerV
         Log.d(tag, "result ==> " + resultString);
 
         if (resultString.length() != 0) {
+
+            sentValueToArduino();
+
             zXingScannerView.stopCamera();
             finish();
         }
 
         zXingScannerView.resumeCameraPreview(ScanCodeActivity.this);
+
+    }
+
+    private void sentValueToArduino() {
+
+        try {
+
+            Log.d(tag, "Value Sent to Arduino ==> " + resultString);
+            showMessage("Value Sent to Arduino ==> " + resultString);
+
+//            resultString = "3";
+
+            boolean b = physicaloid.open();
+            showMessage("physicaloid.open ==> " + b);
+
+            if (b) {
+
+                physicaloid.addReadListener(new ReadLisener() {
+                    @Override
+                    public void onRead(int i) {
+
+                        byte[] bytes = new byte[i];
+                        physicaloid.read(bytes, i);
+
+
+                    }
+                });
+
+
+            } else {
+                showMessage("physicaloid Close");
+            }
+
+
+            byte[] bytes = resultString.getBytes();
+            physicaloid.write(bytes, bytes.length);
+
+        } catch (Exception e) {
+            showMessage("Error ==> " + e);
+        }
+
+
+
+
+    }   // sentValue
+
+    private void showMessage(String messageString) {
+
+        Toast.makeText(ScanCodeActivity.this, messageString,
+                Toast.LENGTH_LONG).show();
 
     }
 
